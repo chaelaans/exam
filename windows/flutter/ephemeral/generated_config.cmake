17017f2b035d50ed9a865b296b0daf2fd71a9e79
chaelaans <161666111+chaelaans@users.noreply.github.com>
2024-05-27 23:04:19 +0700

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\youtube-gk-http-flutter-main" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter\\flutter"
+  "PROJECT_DIR=D:\\youtube-gk-http-flutter-main"
+  "FLUTTER_ROOT=C:\\flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\youtube-gk-http-flutter-main\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\youtube-gk-http-flutter-main"
+  "FLUTTER_TARGET=D:\\youtube-gk-http-flutter-main\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9jNGNkNDhlMTg2NDYwYjMyZDQ0NTg1Y2UzYzEwMzI3MWFiNjc2MzU1Lw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\youtube-gk-http-flutter-main\\.dart_tool\\package_config.json"
+)
